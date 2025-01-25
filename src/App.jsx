158cbc2886c17c410660dd893ae0db3000a8fdbb
Khushil-21 <khushilshah2105@gmail.com>
2025-01-25 12:32:54 +0530

    added tailwind custom var

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
 
 export default function App() {
-	return <div className="text-7xl font-bold text-orange-500">Welcome to Awwwards</div>;
+	return <div className="text-7xl font-bold text-violet-300">Welcome to Awwwards</div>;
 }
