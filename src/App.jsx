f0f4cab6f2af9d311fd1a077c3c7678ffa568cb0
Khushil-21 <khushilshah2105@gmail.com>
2025-01-28 08:38:04 +0530

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
+import NavBar from "./components/NavBar";
 
 export default function App() {
   return <main className="relative min-h-dvh min-w-dvw">
+    <NavBar />
     <HeroSection />
     <AboutSection />
   </main>
