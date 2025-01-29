ce2a9eaddb52bcdcb06c4b4c42d1db81eb27180d
Khushil-21 <khushilshah2105@gmail.com>
2025-01-29 20:38:07 +0530

    story + contact + footer all three sections completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import NavBar from "./components/NavBar";
 import FeaturesSection from "./components/FeaturesSection";
+import StorySection from "./components/StorySection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <main className="relative min-h-dvh min-w-dvw">
-    <NavBar />
-    <HeroSection />
-    <AboutSection />
-    <FeaturesSection />
-  </main>
+	return (
+		<main className="relative min-h-dvh min-w-dvw">
+			<NavBar />
+			<HeroSection />
+			<AboutSection />
+			<FeaturesSection />
+			<StorySection />
+			<ContactSection />
+			<Footer />
+		</main>
+	);
 }
-
