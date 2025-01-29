d356909793991ec9274c0d841cb5a2dec50e54b4
Khushil-21 <khushilshah2105@gmail.com>
2025-01-29 20:04:37 +0530

    FeaturesSection: Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import HeroSection from "./components/HeroSection";
 import AboutSection from "./components/AboutSection";
 import NavBar from "./components/NavBar";
+import FeaturesSection from "./components/FeaturesSection";
 
 export default function App() {
   return <main className="relative min-h-dvh min-w-dvw">
     <NavBar />
     <HeroSection />
     <AboutSection />
+    <FeaturesSection />
   </main>
 }
 
